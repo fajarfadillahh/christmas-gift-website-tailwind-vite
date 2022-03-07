9fd2c0ca70bfb93f0f5e6756d0d04f32b8afe8ea
fajarfadillahh <fajaragustian979@gmail.com>
2022-03-07 22:01:54 +0700

    setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,38 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "767px",
+      lg: "968px",
+      xl: "1024px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#161212",
+      grey: "#5b5757",
+      red: "#da2535",
+    },
+    fontFamily: {
+      body: ["Poppins", "sans-serif"],
+    },
+    fontWeight: {
+      reguler: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+    },
+    lineHeight: {
+      title: "130%",
+      text: "190%",
+    },
   },
   plugins: [],
 };
