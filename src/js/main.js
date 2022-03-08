4b2f477ed90e71e396ab4ce6804930f8668b7b7c
fajarfadillahh <fajaragustian979@gmail.com>
2022-03-08 14:15:21 +0700

    header section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 import "../css/index.css";
+
+// ===== Show Menu =====
+function showMenu(menuId, toggleId) {
+  const menu = document.getElementById(menuId);
+  const toggle = document.getElementById(toggleId);
+
+  toggle.onclick = () => {
+    menu.classList.toggle("show-menu");
+  };
+}
+showMenu("header-menu", "header-toggle");
